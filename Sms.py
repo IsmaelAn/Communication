f1a0391e612c1878ec60d744d1d88e4403b37202
IsmaelAn <ismantunes@hotmail.com>
2021-03-14 00:23:16 +0000

    small correction

--- Sms.py
+++ Sms.py
@@ -7,6 +7,6 @@ auth_token = 'Account Token'            #save them as Env Variables
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-    to="the number you want to send the SMS",
+    to="the number you want to send the SMS to",
     from_="YOUR TWILIO PHONE NUMBER",
     body="MESSAGE")
